4acd4e0f11b0a40929987f206dcdab46d5461757
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2014-06-10 09:30:24 -0400

    extended datagrid cells with potential overflow have an expand effect on double click
    Related Work Items: #965

--- RAMP/src/js/RAMP/Modules/datagrid.js
+++ RAMP/src/js/RAMP/Modules/datagrid.js
@@ -1,4 +1,4 @@
-﻿/*global define, tmpl, TimelineLite */
+﻿/*global define, tmpl, TimelineLite, TweenLite, window */
 /*jslint white: true */
 
 /**
@@ -393,7 +393,7 @@ define([
 
                             console.log("subPanleDock");
                         })
-                        .on("order", function() {
+                        .on("order", function () {
                             topic.publish(EventManager.GUI.SUBPANEL_DOCK, { origin: "datagrid,ex-datagrid" });
 
                             console.log("subPanleDock");
@@ -563,6 +563,45 @@ define([
                             },
 
                             activeClass: "background-light",
+                            useAria: false
+                        }
+                    );
+
+                    popupManager.registerPopup(sectionNode, "hover, focus",
+                        function (d) { d.resolve(); },
+                        {
+                            handleSelector: ".full-table tr",
+                            activeClass: "background-light",
+                            useAria: false
+                        }
+                    );
+
+                    popupManager.registerPopup(sectionNode, "dblclick",
+                        function (d) {
+                            var //oldHandles = jqgrid.find(".expand-cell"),
+                                extraPadding = (this.handle.outerHeight() - this.target.height()) / 2;
+
+                            TweenLite.set(".expand-cell", { clearProps: "padding", className: "-=expand-cell" });
+                            //TweenLite.set(".expand-cell", { className: "-=expand-cell" });
+                            TweenLite.set(this.handle, { padding: extraPadding });
+
+                            window.getSelection().removeAllRanges();
+
+                            d.resolve();
+                        },
+                        {
+                            handleSelector: "td",
+
+                            targetSelector: ".title-span",
+
+                            closeHandler: function (d) {
+                                TweenLite.set(this.handle, { clearProps: "padding" });
+                                TweenLite.set(this.handle, { className: "-=expand-cell" });
+
+                                d.resolve();
+                            },
+
+                            activeClass: "expand-cell",
                             useAria: false
                         }
                     );
@@ -890,7 +929,7 @@ define([
                         }
                     }
                 };
-            } ());
+            }());
 
         /**
         * Caches the sorted data from datatables for feature click events to consume.  Builds featureToPage as a
