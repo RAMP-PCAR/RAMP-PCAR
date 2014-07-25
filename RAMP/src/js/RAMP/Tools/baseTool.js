d8af31c926b276f9b5288d55bc62b3f8de941804
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2014-07-25 15:10:29 -0400

    Add BaseTool

---
+++ RAMP/src/js/RAMP/Tools/baseTool.js
@@ -0,0 +1,28 @@
+﻿/* global define */
+
+/**
+* @module Tools
+*/
+
+/**
+* [Description]
+*
+*
+*
+* @class BaseTool
+* @static
+* @uses dojo/Evented
+* @uses dojo/_base/lang
+* @uses Checkbox
+*/
+
+define(["dojo/Evented", "dojo/_base/lang"],
+    function (Evented, dojoLang) {
+        "use strict";
+
+        return dojoLang.mixin(new Evented(),
+            {
+            }
+        );
+    }
+);
