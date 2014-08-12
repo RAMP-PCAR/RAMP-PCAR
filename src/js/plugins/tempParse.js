1c365b56d5b65c5c88398d827fe47f1502aac76d
Aly Merchant <aly.merchant@ec.gc.ca>
2014-08-12 15:00:01 -0400

    degree symbol not being transferred correctly in plain text

--- src/js/plugins/tempParse.js
+++ src/js/plugins/tempParse.js
@@ -4,9 +4,9 @@ console.log('loading tempParse');
 RAMP.plugins.featureInfoParser.tempParse = function (data) {
     "use strict";
 
-    var val = data.match(/value=(.*)\n/),
+    var val = data.match(/value=(-?\d+\.?\d?)\d*\n/),
         unit = data.match(/unit=(.*)\n/);
     val = val ? val[1] : '';
     unit = unit ? unit[1] : '';
-    return "<p>{0}{1}</p>".format(val, unit);
+    return "<p>{0} &deg;C</p>".format(val, unit);
 };
