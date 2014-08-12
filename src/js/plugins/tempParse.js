d0579b618decddedf3145920889486b865b557ae
Aly Merchant <aly.merchant@ec.gc.ca>
2014-08-12 13:25:46 -0400

    parse only the temperature value for the air temp WMS

---
+++ src/js/plugins/tempParse.js
@@ -0,0 +1,8 @@
+﻿console.log('loading tempParse');
+RAMP.plugins.featureInfoParser.tempParse = function (data) {
+    var val = data.match( /value=(.*)\n/ ),
+        unit = data.match( /unit=(.*)\n/ );
+    val = val ? val[1] : '';
+    unit = unit ? unit[1] : '';
+    return "<p>{0}{1}</p>".format(val,unit);
+}
