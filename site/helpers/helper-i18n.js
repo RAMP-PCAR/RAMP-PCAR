1c54ccd6f9c5b9d62008dc8ba7a7d8894dbc53d2
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2014-09-11 15:18:27 -0400

    Add handelbars templates for a simple test page

---
+++ site/helpers/helper-i18n.js
@@ -0,0 +1,30 @@
+module.exports.register = function(Handlebars, options) {
+
+    var i18n = require('i18next'),
+        file = require('fs-utils'),
+        languages = options.i18next.languages,
+        debug = options.i18next.debug,
+        countryCode = options.i18next.countryCode,
+        localePath = options.i18next.localePath,
+        resStore = {};
+
+    languages.forEach(function(lang) {
+        resStore[lang + '-' + countryCode] = {
+            translation: file.readDataSync(localePath + '/' + lang + "-" + countryCode + '/translation.json')
+        }
+    });
+
+    i18n.init({
+        resStore: resStore,
+        fallbackLng: false,
+        debug: debug
+    });
+
+    Handlebars.registerHelper('t', function(i18n_key, lang) {
+        var result;
+        i18n.setLng(lang + '-' + countryCode);
+        result = i18n.t(i18n_key);
+        //console.log(i18n_key, result, lang);
+        return new Handlebars.SafeString(result);
+    });
+};
