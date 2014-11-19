3b03a67a68dbbcecf9fc690cfe44229c9620757a
Aly Merchant <aly.merchant@ec.gc.ca>
2014-11-19 10:31:40 -0500

    New default parsers for standard getFeatureInfo responses

---
+++ src/js/plugins/jsonRawParse.js
@@ -0,0 +1,8 @@
+﻿/* global RAMP, console */
+
+console.log('loading jsonRawParse');
+RAMP.plugins.featureInfoParser.jsonRawParse = function (data) {
+    "use strict";
+
+    return "<p>{0}</p>".format(data);
+};
