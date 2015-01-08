dedfdd86159fa0a19e867721436111925b46bdf3
Aly Merchant <aly.merchant@ec.gc.ca>
2015-01-08 09:17:35 -0500

    add file path for loading geojson

--- src/js/RAMP/Modules/dataLoader.js
+++ src/js/RAMP/Modules/dataLoader.js
@@ -1,4 +1,6 @@
-﻿/**
+﻿/* global define, Terraformer */
+
+/**
 * A module for loading from web services and local files.  Fetches and prepares data for consumption by the ESRI JS API.
 * 
 * @module RAMP
@@ -6,36 +8,58 @@
 */
 
 define([
-        "dojo/Deferred", "ersi/request"
+        "dojo/Deferred", "ersi/request", "esri/layers/FeatureLayer", "util/util"
     ],
     function (
-            Deferred, EsriRequest
+            Deferred, EsriRequest, FeatureLayer, Util
         ) {
         "use strict";
 
-        function addGeoJson(args) {
+        function makeGeoJsonLayer(geoJson) {
+            var esriJson;
+            esriJson = Terraformer.arcgis.convert(geoJson);
+            return new FeatureLayer(esriJson, { mode: FeatureLayer.MODE_SNAPSHOT });
+
+        }
+
+        function buildGeoJson(args) {
+            var def = new Deferred();
+
             if (args.file) {
                 if (args.url) {
                     throw new Error("Either url or file should be specified, not both");
                 }
-                throw new Error("to be implemented");
-            } else if (args.url) {
-                var def = new Deferred(),
-                    req = new EsriRequest({ url: args.url });
 
-                req.then(function (result) {
+                Util.readFileAsText(args.file).then(function (data) {
                     var jsonLayer = null;
                     try {
-                        jsonLayer = makeGeoJSONLayer(result);
+                        jsonLayer = makeGeoJsonLayer(JSON.parse(data));
+                        def.resolve(jsonLayer);
+                    } catch (e) {
+                        def.reject(e);
+                    }
+                });
+
+                return def.promise;
+
+            } else if (args.url) {
+                (new EsriRequest({ url: args.url })).then(function (result) {
+                    var jsonLayer = null;
+                    try {
+                        jsonLayer = makeGeoJsonLayer(result);
                         def.resolve(jsonLayer);
                     } catch (e) {
                         def.reject(e);
                     }
                 }, function (error) {
-                    def.reject(e);
+                    def.reject(error);
                 });
 
                 return def.promise;
             }
         }
+
+        return {
+            makeGeoJson: buildGeoJson
+        };
     });
