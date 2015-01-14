1db35ca15ddd5a4474bf090991241eaf5f090f9a
Aly Merchant <aly.merchant@ec.gc.ca>
2015-01-14 14:08:13 -0500

    add default renderers

--- src/js/RAMP/Modules/dataLoader.js
+++ src/js/RAMP/Modules/dataLoader.js
@@ -15,12 +15,65 @@ define([
         ) {
         "use strict";
 
-        function makeGeoJsonLayer(geoJson) {
+        var defaultRenderers = {
+            circlePoint: {
+                renderer: {
+                    type: "simple",
+                    symbol: {
+                        type: "esriSMS",
+                        style: "esriSMSCircle",
+                        color: [67, 100, 255, 70],
+                        size: 7
+                    }
+                }
+            },
+            solidLine: {
+                renderer: {
+                    type: "simple",
+                    symbol: {
+                        type: "esriSLS",
+                        style: "esriSLSSolid",
+                        color: [90, 90, 90, 200],
+                        width: 2
+                    }
+                }
+
+            },
+            outlinedPoly: {
+                renderer: {
+                    type: "simple",
+                    symbol: {
+                        type: "esriSFS",
+                        style: "esriSFSSolid",
+                        color: [115,76,0,255],
+                        outline: {
+                            type: "esriSLS",
+                            style: "esriSLSSolid",
+                            color: [110,110,110,255],
+                            width: 1
+                        }
+                    }
+                }
+            }
+
+        };
+
+
+        function makeGeoJsonLayer(geoJson, opts) {
             var esriJson, layerDefinition, layer;
             layerDefinition = {
                 fields: []
             };
+            if (opts) {
+                if (opts.idField) {
+                    layerDefinition.objectIdField = opts.idField;
+                }
+                if (opts.renderer && defaultRenderers.hasOwnProperty(opts.renderer)) {
+                    layerDefinition.drawingInfo = defaultRenderers[opts.renderer];
+                }
+            }
             esriJson = Terraformer.ArcGIS.convert(geoJson);
+            console.log("some esri json");
             console.log(esriJson);
             layer = new FeatureLayer({ layerDefinition: layerDefinition, featureSet: { features: esriJson, geometryType: "esriGeometryPolygon" } }, { mode: FeatureLayer.MODE_SNAPSHOT });
             layer.ramp = { type: "newtype?" };
