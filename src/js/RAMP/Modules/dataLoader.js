3d3e75c964d91df6d190f49e190aa121c4f93c81
Aly Merchant <aly.merchant@ec.gc.ca>
2015-01-17 14:15:00 -0500

    use new shapefile library

--- src/js/RAMP/Modules/dataLoader.js
+++ src/js/RAMP/Modules/dataLoader.js
@@ -1,4 +1,4 @@
-﻿/* global define, console, Terraformer */
+﻿/* global define, console, Terraformer, shp */
 
 /**
 * A module for loading from web services and local files.  Fetches and prepares data for consumption by the ESRI JS API.
@@ -94,33 +94,50 @@ define([
         }
 
         function buildShapefile(args) {
-            var def = new Deferred(), shpArgs, shape;
+            var def = new Deferred();
 
-            if (args.shpFile) {
-                if (args.shpUrl) {
-                    throw new Error("Either shpFile or shpUrl must be specified");
+            if (args.file) {
+                if (args.url) {
+                    throw new Error("Either url or file must be specified");
                 }
-                shpArgs = {shp:args.shpFile};
-                if (args.dbfFile) {
-                    shpArgs.dbf = args.dbfFile;
-                }
-            } else if (args.shpUrl) {
-                shpArgs = {shp:args.shpUrl};
-                if (args.dbfUrl) {
-                    shpArgs.dbf = args.dbfUrl;
-                }
+
+                Util.readFileAsArrayBuffer(args.file).then(function (data) {
+                    var jsonLayer = null;
+                    try {
+                        shp(data).then(function (geojson) {
+                            try {
+                                jsonLayer = makeGeoJsonLayer(geojson);
+                                def.resolve(jsonLayer);
+                            } catch (e) {
+                                def.reject(e);
+                            }
+                        }, function (failReason) {
+                            def.reject(failReason);
+                        });
+                    } catch (e) {
+                        def.reject(e);
+                    }
+                });
+
+                return def.promise;
+
+            } else if (args.url) {
+                shp(args.url).then(function (geojson) {
+                    var jsonLayer = null;
+                    try {
+                        jsonLayer = makeGeoJsonLayer(geojson);
+                        def.resolve(jsonLayer);
+                    } catch (e) {
+                        def.reject(e);
+                    }
+                }, function (error) {
+                    def.reject(error);
+                });
+
+                return def.promise;
             }
-            if (!shpArgs) {
-                throw new Error("Either shpFile or shpUrl must be specified");
-            }
-            shape = new Shapefile(shpArgs, function (data) {
-                try {
-                    def.resolve(makeGeoJsonLayer(data.geojson));
-                } catch (e) {
-                    def.reject(e);
-                }
-            });
-            return def.promise;
+
+            throw new Error("Either url or file must be specified");
         }
 
         function buildGeoJson(args) {
@@ -164,6 +181,7 @@ define([
 
         return {
             makeGeoJsonLayer: makeGeoJsonLayer,
+            buildShapefile: buildShapefile,
             buildGeoJson: buildGeoJson
         };
     });
