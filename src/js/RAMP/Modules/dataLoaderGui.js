fbd180caa18c361ee47a998e82cfb26e244d8254
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-01-23 14:10:36 -0500

    Interaction logic mostly working

--- src/js/RAMP/Modules/dataLoaderGui.js
+++ src/js/RAMP/Modules/dataLoaderGui.js
@@ -25,7 +25,7 @@ define([
 
         console.log(lang, layer_selector_template, TmplHelper, TmplUtil, UtilArray, UtilDict);
 
-        var transitionDuration = 1;
+        var transitionDuration = 0.4;
 
         PopupManager.registerPopup($("#searchMapSectionBody"), "click",
             function (d) {
@@ -35,7 +35,14 @@ define([
                     option = options.find("> ." + this.handle.data("option")),
                     optionStepContent = option.find("> .step-content"),
 
+                    otherOptionButtons = optionsContainer.parents(".step:first").find(".button-pressed"),
+
                     currentOptionsContainer = $("#searchMapSectionBody").find(".current-step"),
+
+                    descendantOptionsContainers,
+                    descendantStagger,
+
+                    optionsLeftShift = optionStepContent.position().left,
 
                     tl = new TimelineLite({ paused: true });
 
@@ -46,33 +53,17 @@ define([
                     tl
                         .to(optionsBackground, 0, { height: optionStepContent.outerHeight() }, 0)
 
-                        .set(options, { left: -optionStepContent.position().left, ease: "easeOutCirc" })
+                        .set(options, { left: -optionsLeftShift, ease: "easeOutCirc" })
 
                         .set(option, { className: '+=active-option' }, 0)
 
                         .to(optionsContainer, 0, { height: optionStepContent.outerHeight(), ease: "easeOutCirc" }, 0)
-                        .fromTo(optionsContainer, transitionDuration, { top: -optionsContainer.height() }, { top: 0, ease: "easeOutCirc" }, 0)
-                    
+                        .fromTo(optionsContainer, transitionDuration, { top: -optionsContainer.height() }, { top: 0, ease: "easeOutCirc" }, 0)                    
                     ;
 
-                    //  this.target.show();
-
-                    //$("#searchMapSectionBody").find(".current-step")
-                    //    .height("auto")
-                    //    .removeClass("current-step");
-
-                    //TweenLite.to(options, 0, { left: -optionStepContent.position().left, ease: "easeOutCirc" });
-                    //  TweenLite.to(this.target, 0, { height: optionStepContent.outerHeight() + 1, ease: "easeOutCirc" });
-                    //  TweenLite.fromTo(this.target, transitionDuration, { top: -this.target.height() }, { top: 0, ease: "easeOutCirc" });
-
-                    //this.target.addClass("current-step");
-
                 } else {
-                    console.log(optionStepContent.position());
-
-                    var //q = this.target.find(".step-options-container:first"),
-                        descendantOptionsContainers = optionsContainer.find(".step-options-container:visible"),
-                        optionsLeftShift = optionStepContent.position().left;
+                    descendantOptionsContainers = optionsContainer.find(".step-options-container:visible");
+                    descendantStagger = transitionDuration / 2 / descendantOptionsContainers.length;
 
                     descendantOptionsContainers.each(function (i, doc) {
                         var docActiveOption,
@@ -84,8 +75,8 @@ define([
 
                         tl
                             .to(doc, transitionDuration,
-                                { top: -docActiveOptionContent.outerHeight(), ease: "easeInCirc" },
-                                transitionDuration * 0.5 * (descendantOptionsContainers.length - i - 1))
+                                { top: -docActiveOptionContent.outerHeight(), ease: "easeOutCirc" },
+                                descendantStagger * (descendantOptionsContainers.length - i - 1))
                             .set(doc, { display: "none" })
                         ;
                     });
@@ -93,36 +84,20 @@ define([
                     if (optionsLeftShift !== options.position().left) {
                         tl
                             .addLabel("leftShiftStart")
-                            .to(optionsBackground, transitionDuration, { height: optionStepContent.outerHeight() }, "leftShiftStart")
+                            .to(optionsBackground, transitionDuration, { height: optionStepContent.outerHeight() }, "leftShiftStart-=0.1")
                             .to(options, transitionDuration,
-                                { left: -optionsLeftShift, ease: "easeOutCirc" }, "leftShiftStart")
+                                { left: -optionsLeftShift, ease: "easeOutCirc" }, "leftShiftStart-=0.1")
                         ;
                     }
-
-                    //tl.play();
-                    //tl
-                    //    .to(this.target.find(".step-options-container"), 0.3, { display: "none", ease: "easeOutCirc" });
-
-                    //TweenLite.to(options, 0.4, { left: -optionStepContent.position().left, ease: "easeOutCirc" });
-                    //TweenLite.to(this.target, 0.4, { height: optionStepContent.outerHeight(), ease: "easeOutCirc" });
-
-                    //TweenLite
                 }
 
                 tl
                     .set(currentOptionsContainer, { height: "auto", className: "-=current-step" }, 0)
                     .set(optionsContainer, { className: "+=current-step" }, 0)
-
+                    .set(otherOptionButtons, { className: "-=button-pressed" }, 0)
                 ;
 
                 tl.play();
-
-                //$("#searchMapSectionBody").find(".current-step")
-                //        .removeClass("current-step");
-
-                //this.target.addClass("current-step");
-
-                this.target.parents(".step:first").find(".button-pressed").removeClass("button-pressed");
 
                 d.resolve();
             },
