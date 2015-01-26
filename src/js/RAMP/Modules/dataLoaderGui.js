0019d6320c78bbff2c442d148907f8b4f5012e16
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-01-26 15:39:46 -0500

    Brain dump 3

--- src/js/RAMP/Modules/dataLoaderGui.js
+++ src/js/RAMP/Modules/dataLoaderGui.js
@@ -27,26 +27,8 @@ define([
         console.log(lang, layer_selector_template, TmplHelper, TmplUtil, UtilArray, UtilDict);
 
         var rootNode = $("#searchMapSectionBody"),
+
             transitionDuration = 0.4;
-
-        function someFunction(control) {
-            var def = new Deferred();
-
-            switch (control.data("action")) {
-                case "featureURL":
-
-                    control.parents(".step-options-container:first").addClass("loading");
-
-                    window.setTimeout(function () {
-                        def.resolve();
-                    }, 1000);
-
-                    break;
-
-            }
-
-            return def.promise;
-        }
 
         PopupManager.registerPopup(rootNode, "click",
             function (d) {
@@ -56,85 +38,222 @@ define([
                     option = options.find("> ." + this.handle.data("option")),
                     optionStepContent = option.find("> .step-content"),
 
-                    otherOptionButtons = optionsContainer.parents(".step:first").find(".button-pressed"),
+                    otherOptionButtons = optionsContainer.prev().find(".button-pressed"),
 
+                    currentStepContent = optionsContainer.prev(),
                     currentOptionsContainer = rootNode.find(".current-step"),
 
-                    descendantOptionsContainers,
-                    descendantStagger,
-                    leftShiftStartAdjustment,
+                    retreatOptionsContainers,
+                    retreatStagger,
 
-                    optionsLeftShift = optionStepContent.position().left,
+                    advanceOptionsContainers = [],
+                    advanceStagger,
+                    lastContainer = optionsContainer,
+
+                    optionsLeftShift,
+                    leftShiftStartAdjustment,
 
                     pr,
 
-                    tl = new TimelineLite({ paused: true });
+                    tl = new TimelineLite({ paused: true }),
 
-                if (this.handle.data("action")) {
-                    pr = someFunction(this.handle, optionsContainer);
-                } else {
-                    pr = (new Deferred()).resolve();
+                    node // temp variable
+                ;
+
+                // find all downstream containers that have active options including the current container;
+                // they will be opened
+                //node = optionsContainer.find(".active-option:first");
+                node = options.find("> ." + this.handle.data("option"));
+                while (node.length) {
+                    advanceOptionsContainers.push(node.parents(".step-options-container:first"));
+                    node = node.find(".active-option:first");
                 }
 
-                pr.then(function () {
-                    if (optionsContainer.is(":hidden")) {
+                // find all downstream containers that are visible;
+                // they will be closed
+                retreatOptionsContainers = optionsContainer.find(".step-options-container:visible");
 
-                        TweenLite.set(optionsContainer, { display: "block" });
+                //function _advance(optionsBackground, optionStepContent, options, option, aoc) {
+                //    tl
+                //        .to(optionsBackground, 0, { height: optionStepContent.outerHeight() }, 0)
+
+                //        .set(options, { left: -optionsLeftShift, ease: "easeOutCirc" })
+
+                //        .set(option, { className: "+=active-option" }, 0)
+
+                //        .to(aoc, 0, { height: optionStepContent.outerHeight(), ease: "easeOutCirc" }, 0)
+                //        .fromTo(aoc, transitionDuration, { top: -aoc.height() }, { top: 0, ease: "easeOutCirc" }, 0)
+                //    ;
+                //}
+
+                function advance() {
+
+                    //var lastContainer;
+
+                    // convert to jQuery array
+                    advanceOptionsContainers = advanceOptionsContainers.map(function (a) { return $(a); });
+                    advanceStagger = transitionDuration / 2 / advanceOptionsContainers.length;
+
+                    option.addClass("active-option");
+
+                    tl.addLabel("advanceStart");
+
+                    advanceOptionsContainers.forEach(function (aoc, i) {
+                        var optionsBackground = aoc.find("> .options-bg"),
+                            options = aoc.find("> .step-options"),
+                            //option = options.find("> ." + this.handle.data("option")),
+                            option = options.find(".active-option:first"),
+                            optionStepContent = option.find("> .step-content");
+
+                        TweenLite.set(aoc, { display: "block" });
 
                         // re-detect the left offset if the block has been hidden before; otherwise it will be zero;
                         optionsLeftShift = optionStepContent.position().left;
 
                         tl
-                            .to(optionsBackground, 0, { height: optionStepContent.outerHeight() }, 0)
+                            .to(optionsBackground, 0, { height: optionStepContent.outerHeight() }, "advanceStart+=" + advanceStagger * (i))
 
-                            .set(options, { left: -optionsLeftShift, ease: "easeOutCirc" })
+                            .set(options, { left: -optionsLeftShift, ease: "easeOutCirc" }, "advanceStart+=" + advanceStagger * (i))
 
-                            .set(option, { className: '+=active-option' }, 0)
+                            //.set(option, { className: "+=active-option" }, 0)
 
-                            .to(optionsContainer, 0, { height: optionStepContent.outerHeight(), ease: "easeOutCirc" }, 0)
-                            .fromTo(optionsContainer, transitionDuration, { top: -optionsContainer.height() }, { top: 0, ease: "easeOutCirc" }, 0)
+                            .to(aoc, 0, { height: optionStepContent.outerHeight(), ease: "easeOutCirc" }, "advanceStart+=" + advanceStagger * (i))
+                            .fromTo(aoc, transitionDuration,
+                                { top: -aoc.height() },
+                                { top: 0, ease: "easeOutCirc" },
+                                "advanceStart+=" + advanceStagger * (i))
+                            .set(aoc, { height: "auto" }, "advanceStart+=" + advanceStagger * (i))
                         ;
 
+                        lastContainer = aoc;
+                    });
+
+                    //tl.set(lastContainer, { className: "+=current-step", immediateRender: false });
+
+                    /*TweenLite.set(optionsContainer, { display: "block" });
+        
+                    // re-detect the left offset if the block has been hidden before; otherwise it will be zero;
+                    optionsLeftShift = optionStepContent.position().left;
+        
+                    tl
+                        .to(optionsBackground, 0, { height: optionStepContent.outerHeight() }, 0)
+        
+                        .set(options, { left: -optionsLeftShift, ease: "easeOutCirc" })
+        
+                        .set(option, { className: "+=active-option" }, 0)
+        
+                        .to(optionsContainer, 0, { height: optionStepContent.outerHeight(), ease: "easeOutCirc" }, 0)
+                        .fromTo(optionsContainer, transitionDuration, { top: -optionsContainer.height() }, { top: 0, ease: "easeOutCirc" }, 0)
+                    ;*/
+                }
+
+                function retreat() {
+                    retreatStagger = transitionDuration / 2 / retreatOptionsContainers.length;
+                    leftShiftStartAdjustment = retreatOptionsContainers.length > 0 ? "-=0.1" : "";
+
+                    retreatOptionsContainers.each(function (i, doc) {
+                        var docActiveOption,
+                            docActiveOptionContent;
+
+                        doc = $(doc);
+                        docActiveOption = doc.find("> .step-options > .step.active-option");
+                        docActiveOptionContent = docActiveOption.find("> .step-content");
+
+                        tl
+                            .to(doc, transitionDuration,
+                                { top: -docActiveOptionContent.outerHeight(), ease: "easeOutCirc" },
+                                retreatStagger * (retreatOptionsContainers.length - i - 1))
+                            .set(doc, { display: "none" })
+                        //.set(docActiveOption, { className: "-=active-option" })
+                        ;
+                    });
+                }
+
+                function shift() {
+
+                    optionsLeftShift = optionStepContent.position().left;
+
+                    tl
+                        .addLabel("leftShiftStart")
+
+                        .to(optionsBackground, transitionDuration, { height: optionStepContent.outerHeight() }, "leftShiftStart" + leftShiftStartAdjustment)
+                        .to(options, transitionDuration,
+                            { left: -optionsLeftShift, ease: "easeOutCirc" }, "leftShiftStart" + leftShiftStartAdjustment)
+                    ;
+                }
+
+                function someFunction(action) {
+                    var def = new Deferred();
+
+                    switch (action) {
+                        case "cancel":
+
+                            retreatOptionsContainers =
+                                optionsContainer.add(
+                                    optionsContainer.find(".step-options-container:visible")
+                                );
+
+                            retreat();
+                            tl.play();
+
+                            d.resolve();
+
+                            break;
+
+                        case "featureURL":
+
+                            currentOptionsContainer.addClass("loading");
+
+                            window.setTimeout(function () {
+                                currentOptionsContainer.removeClass("loading");
+                                currentStepContent.addClass("loaded");
+
+                                def.resolve();
+                            }, 1000);
+
+                            break;
+                    }
+
+                    return def.promise;
+                }
+
+                if (this.handle.data("action")) {
+                    pr = someFunction(this.handle.data("action"));
+                } else {
+                    pr = (new Deferred()).resolve();
+                }
+
+                pr.then(function () {
+
+                    if (optionsContainer.is(":hidden")) {
+                        advance();
                     } else {
-                        descendantOptionsContainers = optionsContainer.find(".step-options-container:visible");
-                        descendantStagger = transitionDuration / 2 / descendantOptionsContainers.length;
-                        leftShiftStartAdjustment = descendantOptionsContainers.length > 0 ? "-=0.1" : "";
+                        //retreatOptionsContainers = optionsContainer.find(".step-options-container:visible");
 
-                        descendantOptionsContainers.each(function (i, doc) {
-                            var docActiveOption,
-                                docActiveOptionContent;
-
-                            doc = $(doc);
-                            docActiveOption = doc.find("> .step-options > .step.active-option");
-                            docActiveOptionContent = docActiveOption.find("> .step-content");
-
-                            tl
-                                .to(doc, transitionDuration,
-                                    { top: -docActiveOptionContent.outerHeight(), ease: "easeOutCirc" },
-                                    descendantStagger * (descendantOptionsContainers.length - i - 1))
-                                .set(doc, { display: "none" })
-                            ;
-                        });
+                        retreat();
 
                         if (optionsLeftShift !== options.position().left) {
-                            tl
-                                .addLabel("leftShiftStart")
-                                .to(optionsBackground, transitionDuration, { height: optionStepContent.outerHeight() }, "leftShiftStart" + leftShiftStartAdjustment)
-                                .to(options, transitionDuration,
-                                    { left: -optionsLeftShift, ease: "easeOutCirc" }, "leftShiftStart" + leftShiftStartAdjustment)
-                            ;
+                            shift();
                         }
+
+                        // drop the first container since it shouldn't be advanced
+                        UtilArray.remove(advanceOptionsContainers, 0);
+                        advance();
                     }
 
                     tl
                         .set(currentOptionsContainer, { height: "auto", className: "-=current-step" }, 0)
-                        .set(optionsContainer, { className: "+=current-step" }, 0)
+                        .set(lastContainer, { className: "+=current-step" }, 0)
+                        //.set(optionsContainer, { className: "+=current-step" }, 0)
                         .set(otherOptionButtons, { className: "-=button-pressed" }, 0)
                     ;
 
                     tl.play();
 
                     d.resolve();
+                }, function (err) {
+                    console.log(err);
+
                 });
             },
             {
