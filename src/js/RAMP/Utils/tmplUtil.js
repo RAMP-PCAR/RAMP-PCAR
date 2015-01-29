added05f17f411463e4bb1db53fc4a55b4967e60
James Rae <james.rae@ec.gc.ca>
2015-01-29 14:35:46 -0500

    enhanced regexes

--- src/js/RAMP/Utils/tmplUtil.js
+++ src/js/RAMP/Utils/tmplUtil.js
@@ -173,13 +173,21 @@ define(["ramp/globalStorage"],
             */
             autoHyperlink: function (content) {
                 //http://stackoverflow.com/questions/11863847/regex-to-match-urls-but-not-urls-in-hyperlinks
+                //http://stackoverflow.com/questions/15039993/javascript-function-to-find-email-address-from-a-webpage
 
+                // orig regex's.  did not handle pre-linked content
                 // var urlRegex = /((f|ht)tp(s|):\/\/.+?[\w=%\?\&\./-]+)/g;
-                var urlRegex = /(?:^|[^"'])((ftp|http|https|file):\/\/[\S]+(\b|$))/gi
-                content = content.replace(urlRegex, '<a href="$1">$1</a>');
+                // var emailRegex = /([\w-\.]+@([\w-]+\.)+[\w-]{2,4})/g;
 
-                var emailRegex = /([\w-\.]+@([\w-]+\.)+[\w-]{2,4})/g;
-                content = content.replace(emailRegex, '<a href="mailto:$1">$1</a>')
+                var urlRegex = /(["'>:]?)((ftp|http|https|file):\/\/[\S]+(\b|$))/gi;
+                content = content.replace(urlRegex, function ($0, $1) {
+                    return $1 ? $0 : '<a href="' + $0 + '" target="_blank">' + $0 + '</a>';
+                });
+
+                var emailRegex = /(["'>:]?)([\w.-]+@[\w.-]+\.[\w.-]+)/gi;
+                content = content.replace(emailRegex, function ($0, $1) {
+                    return $1 ? $0 : '<a href="mailto:' + $0 + '">' + $0 + '</a>';
+                });
 
                 return content;
             },
