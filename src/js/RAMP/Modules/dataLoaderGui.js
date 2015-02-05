ccb50a271bff66b659f3539a5b94c603d1c9c0d8
Aleksuei Riabtsev <aleksuei.riabtsev@ec.gc.ca>
2015-02-05 15:01:25 -0500

    Move hex to rgb converted to Util module

--- src/js/RAMP/Modules/dataLoaderGui.js
+++ src/js/RAMP/Modules/dataLoaderGui.js
@@ -657,24 +657,9 @@ define([
             return tmpl(key, data);
         }
 
-        function hexToRgb(hex) {
-            // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-            var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-            hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-                return r + r + g + g + b + b;
-            });
-
-            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-            return result ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16)
-            } : null;
-        }
-
         function addCSVDataset(obj) {
             var promise,
-                rgbColour = hexToRgb(obj.colour),
+                rgbColour = UtilMisc.hexToRgb(obj.colour),
                 iconTemplate = _template("a_d_icon_circlePoint", obj);
 
             console.log(iconTemplate);
--- src/js/RAMP/Utils/util.js
+++ src/js/RAMP/Utils/util.js
@@ -1237,6 +1237,29 @@ define(["dojo/_base/array", "dojo/_base/lang", "dojo/topic", "dojo/Deferred", "e
                 console.log(type + " delimiter detected: '" + (detected || delimiters[type][0]) + "'");
 
                 return (detected || delimiters[type][0]);
+            },
+
+            /**
+            * Converts HEX colour to RGB.
+            * http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb/5624139#5624139
+            *
+            * @method hexToRgb
+            * @static
+            * @return {Object} ojbect containing r, g, and b components of the supplied colour
+            */
+            hexToRgb: function (hex) {
+                // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+                var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+                hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+                    return r + r + g + g + b + b;
+                });
+
+                var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+                return result ? {
+                    r: parseInt(result[1], 16),
+                    g: parseInt(result[2], 16),
+                    b: parseInt(result[3], 16)
+                } : null;
             }
         };
     });
