88deeec31deeaca5b0204f21389cf20641680be4
Aly Merchant <aly.merchant@ec.gc.ca>
2015-04-08 08:21:45 -0400

    feat(plugin): tabular JSON query output
    Relates #5729

--- src/js/plugins/jsonParse.js
+++ src/js/plugins/jsonParse.js
@@ -7,7 +7,7 @@ RAMP.plugins.featureInfoParser.jsonParse = function (data) {
 
     data = { a: 1, b: '2', c: true, d: [1, 2, 3], e: { f: 1, g: 2 }, h: null };
 
-    summary = ''.join(Object.keys(data).filter(function (e) { return typeof e !== 'object' && typeof e !== 'undefined'; }).map(function (e) { return '<tr><th>{0}</th><td>{1}</td></tr>'.format(e, data[e]); }));
+    summary = Object.keys(data).filter(function (e) { return typeof data[e] !== 'object' && typeof data[e] !== 'undefined'; }).map(function (e) { return '<tr><th>{0}</th><td>{1}</td></tr>'.format(e, data[e]); }).join('');
 
-    return "<p>{0}</p>".format(data);
+    return "<table>{0}</table>".format(summary);
 };
