6a72a7379a3bb502c0f5502c464da3f1fab7af62
Aly Merchant <aly.merchant@ec.gc.ca>
2015-04-21 18:08:12 -0400

    feat(plugin): full JSON visible in modal popup
    Closes #5729

--- src/js/plugins/jsonParse.js
+++ src/js/plugins/jsonParse.js
@@ -11,7 +11,7 @@ RAMP.plugins.featureInfoParser.jsonParse = function (data, layerId) {
     }
 
     summary = Object.keys(data).filter(function (e) { return typeof data[e] !== 'object' && typeof data[e] !== 'undefined'; }).map(function (e) { return '<tr><th>{0}</th><td style="padding-left: 1em">{1}</td></tr>'.format(e, data[e]); }).join('');
-    txArea = '<textarea style="display: none">{0}</textarea>'.format(JSON.stringify(data));
+    txArea = '<textarea class="json-details" style="display: none">{0}</textarea>'.format(JSON.stringify(data));
 
     return '<table>{0}</table>{1}'.format(summary,txArea);
 };
